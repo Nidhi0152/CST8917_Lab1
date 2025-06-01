449fb0c6600aac4c541ca4c46fbb5c29ae7bfb58
Nidhi0152 <194421451+Nidhi0152@users.noreply.github.com>
2025-05-31 22:31:04 -0400

    Added changes for SQL Binding in functionapp.py

--- function_app.py
+++ function_app.py
@@ -1,27 +1,32 @@
 import azure.functions as func
 import logging
+from azure.functions.decorators.core import DataType
+import uuid
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+app = func.FunctionApp()
 
-@app.route(route="HttpExample")
-@app.queue_output(arg_name="msg", queue_name="outqueue", connection="AzureWebJobsStorage")
-def HttpExample(req: func.HttpRequest, msg: func.Out[func.QueueMessage]) -> func.HttpResponse:
+@app.function_name(name="HttpTrigger1")
+@app.route(route="hello", auth_level=func.AuthLevel.ANONYMOUS)
+@app.generic_output_binding(
+    arg_name="toDoItems",
+    type="sql",
+    CommandText="dbo.ToDo",
+    ConnectionStringSetting="SqlConnectionString",
+    data_type=DataType.STRING
+)
+def test_function(req: func.HttpRequest, toDoItems: func.Out[func.SqlRow]) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
+    name = req.get_json().get('name')
     if name:
-        msg.set(name)
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        toDoItems.set(func.SqlRow({
+            "Id": str(uuid.uuid4()),
+            "title": name,
+            "completed": False,
+            "url": ""
+        }))
+        return func.HttpResponse(f"Hello {name}!")
     else:
         return func.HttpResponse(
-            "Pass a name in the query string or in the request body.",
-            status_code=200
+            "Please pass a name in the request body",
+            status_code=400
         )
